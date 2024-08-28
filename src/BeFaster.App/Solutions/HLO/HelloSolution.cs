c774cca20c78059159d346954a628f3575d2a05f
sbx_user1051 <sbx_user1051@169.254.33.213>
2024-08-28 11:21:01 +0000

    Wed Aug 28 11:21:01 UTC 2024

--- src/BeFaster.App/Solutions/HLO/HelloSolution.cs
+++ src/BeFaster.App/Solutions/HLO/HelloSolution.cs
@@ -6,8 +6,7 @@ namespace BeFaster.App.Solutions.HLO
     {
         public static string Hello(string? friendName)
         {
-            return "Hello, World!";
+            return "Hello, "+friendName+"!";
         }
     }
 }
-
